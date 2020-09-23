f20b91a062814e2caeb28316af3cc8331e3c10d9
zhouzh6 <zhouzh6@asiainfo.com>
2020-09-23 09:28:59 +0800

    剑指offer两题

--- src/剑指Offer/二维数组中的查找.java
+++ src/剑指Offer/二维数组中的查找.java
@@ -1,5 +1,7 @@
 package 剑指Offer;
 
+import java.util.ArrayList;
+
 /**
  * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
@@ -9,17 +11,42 @@ package 剑指Offer;
  * 现有矩阵 matrix 如下：
  *
  * [
- *   [1,   4,  7, 11, 15],
- *   [2,   5,  8, 12, 19],
- *   [3,   6,  9, 16, 22],
- *   [10, 13, 14, 17, 24],
- *   [18, 21, 23, 26, 30]
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
  * ]
  * 给定 target = 5，返回 true。
  *
  * 给定 target = 20，返回 false。
  */
 public class 二维数组中的查找 {
+    public static void main(String[] args) {
+        System.out.println(find(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20));
+    }
+
+    public static boolean find(int[][] array, int target) {
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            return false;
+        }
+        int n = array.length;
+        int m = array[0].length;
+        int row = 0, colum = m - 1;
+        if (target > array[n - 1][m - 1]) {
+            return false;
+        }
+        while (row <= n - 1 && colum <= m - 1) {
+            if (target > array[row][colum]) {
+                row++;
+            } else if (target < array[row][colum]) {
+                colum--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static boolean findNumberIn2DArray(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
